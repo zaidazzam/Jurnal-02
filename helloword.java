4b2f23acce6ea88aa28f664bc0de2c7d4621e959
zaidazzam <azzamzaid10@gmail.com>
2021-09-26 10:30:30 +0700

    proyek kahiji

---
+++ helloword.java
@@ -0,0 +1,9 @@
+public class HelloWord {
+
+    public static void main(String [] args) {
+        System.out.println ("Nama: Zaid Abdullah Azzam");
+		System.out.println ("NIM:6706213055");
+		System.out.println ("kelas: 45-01");
+    }
+
+}
